93a767fd4c2c30ff68e99d13f24b04ed2d11b2d9
Wedlec129 <boryasokolov@gmail.com>
2023-08-03 16:45:45 +0300

    Initial Commit

---
+++ fireBaseTest/fireBaseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  fireBaseTestApp.swift
+//  fireBaseTest
+//
+//  Created by Борух Соколов on 03.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct fireBaseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
